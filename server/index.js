25f070cf3570afced4d3280de8432c03c447e0fc
Momoleng <momoleng@Momolengs-MacBook-Pro.local>
2020-09-28 23:50:31 +0700

    build

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ app.use(cors())
 const tasklist = require('./routes/api/tasklist')
 app.use('/api/tasklist', tasklist)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+} 
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`started port ${port}`))
